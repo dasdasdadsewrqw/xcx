f3dbce22832ef199846b1ae709fc35effa9b4ce1
清风 <1689251881@qq.com>
2020-06-01 16:41:40 +0800

    1

--- dt/pages/personal/personal.js
+++ dt/pages/personal/personal.js
@@ -1,4 +1,6 @@
 // pages/personal/personal.js
+//获取应用实例
+const app = getApp()
 Page({
 
   /**
@@ -12,7 +14,9 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    wx.setNavigationBarTitle({
+      title: '个人中心'
+    })
   },
 
   /**
