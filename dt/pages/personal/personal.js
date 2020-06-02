153b3706d80fd5f4498b289f21836e94dac01215
清风 <1689251881@qq.com>
2020-06-02 17:41:43 +0800

    <1689251881@qq.com>

--- dt/pages/personal/personal.js
+++ dt/pages/personal/personal.js
@@ -1,20 +1,22 @@
 // pages/personal/personal.js
 //获取应用实例
-const app = getApp()
+const app = getApp();
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    portrait:'http://img1.imgtn.bdimg.com/it/u=3995885402,1113893169&fm=26&gp=0.jpg',
-    name: '刘德华',
+    userList: {
+      avatarUrl: 'http://up.enterdesk.com/edpic/d5/85/f4/d585f486048d9e8ac43b55bc389a709c.jpg',
+    },
     vip: '青铜选手',
     integral: '2000',
     ddNumber: '2000',
     ddName: '订单数量',
     xdMoney: '60000',
     xdTitle: '下单金额',
+    loginStatus: false,
   },
 
   /**
@@ -23,6 +25,54 @@ Page({
   onLoad: function (options) {
     wx.setNavigationBarTitle({
       title: '个人中心'
+    });
+    let that = this;
+    wx.getSetting({
+      success (res){
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称
+          wx.getUserInfo({
+            success: function(res) {
+              that.setData({
+                loginStatus: true,
+                userList: res.userInfo
+              });
+              console.log(res.userInfo)
+            }
+          })
+        } else {
+          console.log('未授权');
+          that.setData({
+            loginStatus:false
+          });
+        }
+      }
+    })
+  },
+  bindGetUserInfo (e) {
+    console.log(e.detail.userInfo)
+    if (e.detail.userInfo) {
+      this.setData({
+        loginStatus: true,
+        userList: e.detail.userInfo
+      });
+    }
+  },
+
+  wxLogon: function() {
+    wx.login({
+      success (res) {
+        console.log(res.code);
+        // if (res.code) {
+        //   //发起网络请求
+        //   wx.request({
+        //     url: 'https://test.com/onLogin', 
+        //     data: {code: res.code},
+        //   })
+        // } else {
+        //   console.log('登录失败！' + res.errMsg)
+        // }
+      }
     })
   },
 
