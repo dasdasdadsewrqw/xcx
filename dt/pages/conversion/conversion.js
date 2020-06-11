efbde3ce881e270a0d524152aaae6dedcea87010
清风 <1689251881@qq.com>
2020-06-11 14:33:47 +0800

    1

--- dt/pages/conversion/conversion.js
+++ dt/pages/conversion/conversion.js
@@ -1,66 +1,44 @@
-// pages/conversion/conversion.js
-Page({
-
-    /**
-     * 页面的初始数据
-     */
+// pages/coupon/coupon.js
+Component({
     data: {
-
+        discounts: '',
     },
+    methods: {
+        /**
+        * 生命周期函数--监听页面加载
+        */
+        onLoad: function(options) {
+            wx.setNavigationBarTitle({
+                title: '优惠券兑换'
+            });
 
-    /**
-     * 生命周期函数--监听页面加载
-     */
-    onLoad: function (options) {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload: function () {
-
-    },
-
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh: function () {
-
-    },
-
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom: function () {
-
-    },
-
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage: function () {
+            // this.setData({
+               
+            // })
+        },
+        formSubmit(e) {
+            let data = e.detail.value;
+            console.log(data);
+            wx.request({
+                url: '/ceshi', 
+                data: data,
+                method: 'POST',
+                success(res) {
+                    console.log(res.data)
+                    wx.showToast({
+                        title: '操作成功',
+                        duration: 2000
+                    })
+                },
+                fail(err) {
+                    wx.showToast({
+                        title: err.errMsg,
+                        icon: "none",
+                        duration: 2000
+                    })
+                }
+            })
+        }
 
     }
 })
